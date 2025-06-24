06586e844b416a2e1b53b779a7addcf8b0ade99a
Nicko <noemail@emails.coms>
2025-06-24 05:40:45 +0300

    third section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import "./App.css";
-
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
+import { Feedback } from "./components/Feedback/Feedback";
+//переделать///////////////////////////////////////////////////////
+import type { Feedback as h } from "./components/Feedback/Feedback";
+import avatar from "./assets/images/feedback/oval.png";
+import stars from "./assets/images/feedback/stars.svg";
+////////////////////////////////////////////////////////////
+const data: h = {
+    imageURL: avatar,
+    ratingImage: stars,
+    text: '"“OMG! I cannot believe that I have got a brand new landing page after getting Albino. It was super easy to edit and publish.”',
+    name: "Franklin Hicks",
+    profession: "Web Developer",
+    altAvatar: "Developer",
+    altRating: "rating",
+};
 function App() {
     return (
         <>
@@ -11,6 +25,7 @@ function App() {
                 <Hero />
             </div>
             <Services />
+            <Feedback imageURL={data.imageURL} ratingImage={data.ratingImage} text={data.text} name={data.name} profession={data.profession} altAvatar={data.altAvatar} altRating={data.altRating} />
         </>
     );
 }
---
+++ src/components/Feedback/Feedback.tsx
@@ -0,0 +1,39 @@
+import styles from "./Feedback.module.scss";
+
+export type Feedback = {
+    imageURL: string;
+    ratingImage: string;
+    text: string;
+    name: string;
+    profession: string;
+    altAvatar: string;
+    altRating: string;
+};
+
+export function Feedback(value: Feedback) {
+    return (
+        <section className={styles.feedback}>
+            <div className={styles.feedback_container}>
+                <div className={styles.left}>
+                    <img src={value.imageURL} alt={value.altAvatar} />
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.rating_container}>
+                        <img
+                            className={styles.rating}
+                            src={value.ratingImage}
+                            alt={value.altRating}
+                        />
+                    </div>
+                    <h4 className={styles.title}>{value.text}</h4>
+                    <div className={styles.author_container}>
+                        <span className={styles.author_name}>{value.name}</span>
+                        <span className={styles.author_profession}>
+                            {value.profession}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
